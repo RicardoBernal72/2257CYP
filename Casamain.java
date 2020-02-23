e1898ace6602a6fb2b7de8ce881f724a02398eed
RicardoBernal72 <55105058+RicardoBernal72@users.noreply.github.com>
2020-02-23 14:12:46 -0600

    Add files via upload
    se crea el repositorio y se sube la tarea.

---
+++ Casamain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion5;
+
+import Casa.componentes.Casa;
+
+/**
+ *
+ * @author kikis
+ */
+public class Casamain {
+    
+    
+    public static void main(String[] args) {
+        Casa Casa1=new Casa();
+        
+        
+    }
+       
+    
+}
